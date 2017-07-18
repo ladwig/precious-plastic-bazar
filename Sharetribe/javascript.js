91708a435026543f676488ce30f9ae43ed88b194
Daniel Ladwig <daniel.ladwig@hotmail.de>
2017-07-18 12:39:29 +0200

    Changed the menu description

--- Sharetribe/javascript.js
+++ Sharetribe/javascript.js
@@ -1,13 +1,2 @@
-
-$(".col-12 ul").text(function () {
-    return $(this).text().replace("Pickup", "Domestic or pickup ");
-});
-
-$(".delivery-title").text(function () {
-    return $(this).text().replace("Delivery method", "Shipping method ");
-});
-
-
-$(".col-12 ul").text(function () {
-    return $(this).text().replace("Shipping", "International");
-});
+let toggleHeader = document.querySelector(".toggle-header");
+toggleHeader.firstChild.nodeValue = "Buy & request";
